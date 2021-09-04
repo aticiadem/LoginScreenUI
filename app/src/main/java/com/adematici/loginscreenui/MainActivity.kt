3576a0736f33afaa95b597544ccbaef5bfc4986c
adematici <adematicis41@gmail.com>
2021-09-04 13:37:43 +0300

    navigation and screens added to project

--- app/src/main/java/com/adematici/loginscreenui/MainActivity.kt
+++ app/src/main/java/com/adematici/loginscreenui/MainActivity.kt
@@ -3,11 +3,20 @@ package com.adematici.loginscreenui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.adematici.loginscreenui.ui.screens.LoginScreen
+import com.adematici.loginscreenui.ui.screens.RegisterScreen
+import com.adematici.loginscreenui.ui.screens.SplashScreen
 import com.adematici.loginscreenui.ui.theme.LoginScreenUITheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +24,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LoginScreenUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Navigation()
                 }
             }
         }
@@ -25,14 +36,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LoginScreenUITheme {
-        Greeting("Android")
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen"){
+        composable("splash_screen"){
+            SplashScreen()
+        }
+        composable("login_screen"){
+            LoginScreen()
+        }
+        composable("register_screen"){
+            RegisterScreen()
+        }
     }
 }
