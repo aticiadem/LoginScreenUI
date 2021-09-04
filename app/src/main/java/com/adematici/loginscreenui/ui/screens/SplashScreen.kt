f5d941457c1a942c55983a56015ac85bcb88c54f
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-09-04 21:49:24 +0300

    splash screen ui completed (#4)

--- app/src/main/java/com/adematici/loginscreenui/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/adematici/loginscreenui/ui/screens/SplashScreen.kt
@@ -1,8 +1,37 @@
 package com.adematici.loginscreenui.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.adematici.loginscreenui.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(){
-
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(key1 = true) {
+        delay(800)
+        navController.navigate("login_screen") {
+            popUpTo("splash_screen") { inclusive = true }
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_google_logo
+            ),
+            contentDescription = "Google Logo",
+            modifier = Modifier.size(200.dp)
+        )
+    }
 }
