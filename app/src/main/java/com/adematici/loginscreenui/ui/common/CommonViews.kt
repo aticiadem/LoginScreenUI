c64fd935d820f7309645e6cc335ada2099747ff1
adematici <adematicis41@gmail.com>
2021-09-04 21:27:01 +0300

    common views added

---
+++ app/src/main/java/com/adematici/loginscreenui/ui/common/CommonViews.kt
@@ -0,0 +1,125 @@
+package com.adematici.loginscreenui.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.adematici.loginscreenui.R
+import com.adematici.loginscreenui.ui.theme.LightGrayColor
+import com.adematici.loginscreenui.ui.theme.PinkColor
+import com.adematici.loginscreenui.ui.theme.SkinColor
+
+@Composable
+fun CommonText(
+    text: String,
+    color: Color = Color.Black,
+    fontSize: TextUnit = 16.sp,
+    fontWeight: FontWeight = FontWeight.Normal
+) {
+    Text(
+        text = text,
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight
+    )
+}
+
+@Composable
+fun CommonLoginButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier
+            .background(
+                Brush.horizontalGradient(
+                    colors = listOf(
+                        PinkColor, SkinColor
+                    )
+                ),
+                RoundedCornerShape(20.dp)
+            )
+            .height(58.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent
+        ),
+        onClick = { onClick() },
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp)
+    ) {
+        Text(text = text, fontSize = 20.sp, color = Color.White)
+    }
+}
+
+@Composable
+fun CommonGoogleButton(
+    text: String
+) {
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White
+        ),
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp),
+        modifier = Modifier
+            .background(Color.White, RoundedCornerShape(20.dp))
+            .fillMaxWidth()
+            .height(58.dp)
+            .border(3.dp, Color.Black, RoundedCornerShape(25.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color.White, RoundedCornerShape(20.dp)
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_google_logo),
+                contentDescription = "Google Logo",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.width(18.dp))
+            Text(
+                text = text,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.W400
+            )
+        }
+    }
+}
+
+@Composable
+fun CommonTextField(
+    text: String,
+    placeholder: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = text,
+        onValueChange = { onValueChange(it) },
+        label = { Text(text = placeholder, color = LightGrayColor) },
+        maxLines = 1,
+        singleLine = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = PinkColor,
+            cursorColor = Color.Black
+        ),
+        shape = RoundedCornerShape(20.dp),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
